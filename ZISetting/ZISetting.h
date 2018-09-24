0188e7ba4ffa7b4bb214022090cb21e79cfe56b8
fauzisho <fauzisholichin@gmail.com>
2018-09-24 14:09:34 +0700

    Initial Commit

---
+++ ZISetting/ZISetting.h
@@ -0,0 +1,19 @@
+//
+//  ZISetting.h
+//  ZISetting
+//
+//  Created by UziApel on 24/09/18.
+//  Copyright © 2018 Uzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZISetting.
+FOUNDATION_EXPORT double ZISettingVersionNumber;
+
+//! Project version string for ZISetting.
+FOUNDATION_EXPORT const unsigned char ZISettingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZISetting/PublicHeader.h>
+
+
